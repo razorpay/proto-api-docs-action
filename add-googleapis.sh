7205729aa327b79e5d7ca3ee363b2d2b4235e3fa
Pranav Gupta <pranavguptarulz@gmail.com>
2022-01-04 19:40:58 +0530

    add files

--- add-googleapis.sh
+++ add-googleapis.sh
@@ -6,7 +6,7 @@ cd /googleapis
 git init
 git config --local gc.auto 0
 git config core.sparseCheckout true
-git remote add origin git@github.com:googleapis/googleapis.git
+git remote add origin https://github.com:googleapis/googleapis
 echo "google/" > .git/info/sparse-checkout
 git -c protocol.version=2 fetch --no-tags --prune --depth=1 origin master
 git checkout master
