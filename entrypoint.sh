36cc497b129d40e7468a7393a3bbda34c15085b4
Pranav Gupta <pranavguptarulz@gmail.com>
2022-04-20 08:12:19 +0530

    Update packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+cd /action
+
 echo "cloning protobuf files"
-bash /sparse-checkout.sh
+bash sparse-checkout.sh
 
 echo "generating swagger docs"
-cd / && buf beta mod update && buf generate
+buf mod update && buf generate
 
 echo "combining swagger docs into 1 file"
-bash /combine_swagger_docs.sh /docs
+bash combine_swagger_docs.sh docs
 
 echo "create repo/branch dir structure"
 mkdir -p /_docs/${GITHUB_REPOSITORY#*/} && mv combined.json /_docs/${GITHUB_REPOSITORY#*/}/${GITHUB_REF##*/}.json
 
 
 echo "upload to s3"
-bash /upload_to_s3.sh
+bash upload_to_s3.sh
