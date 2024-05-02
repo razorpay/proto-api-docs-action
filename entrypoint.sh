2fbaf549c14304b1b14a8b842cea3c0f17bbbcce
Alok Kumar Singh <mail.alok87@gmail.com>
2024-05-02 13:29:33 +0530

    Test new

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,13 @@ echo "cloning protobuf files"
 bash sparse-checkout.sh
 
 echo "generating swagger docs"
-buf mod update && buf generate
+buf mod update && buf generate proto/proto
 
 echo "combining swagger docs into 1 file"
 bash combine_swagger_docs.sh docs
 
 echo "create repo/branch dir structure"
 mkdir -p /_docs/${GITHUB_REPOSITORY#*/} && mv combined.json /_docs/${GITHUB_REPOSITORY#*/}/${GITHUB_REF##*/}.json
-
 
 echo "upload to s3"
 bash upload_to_s3.sh
