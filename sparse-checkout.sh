4e54d586cf77e4fd9d4ca2ad44c815c3fcedd51f
udayraj-rzp <udayraj.deshmukh@razorpay.com>
2025-07-14 18:45:58 +0530

    fix: attempt

--- sparse-checkout.sh
+++ sparse-checkout.sh
@@ -12,6 +12,10 @@ git config --local gc.auto 0
 git config core.sparseCheckout true
 GIT_TOKEN=$INPUT_GIT_TOKEN
 cp $GITHUB_WORKSPACE/${INPUT_MODULE_LIST_FILE_PATH} .git/info/sparse-checkout
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global url."https://x-access-token:${INPUT_GIT_TOKEN}@github.com/".insteadOf https://github.com/ 
 git remote add origin https://${INPUT_GIT_TOKEN}@github.com/${INPUT_PROTO_REPOSITORY}
 git -c protocol.version=2 fetch --no-tags --prune --depth=1 origin ${BRANCH_NAME}
 git checkout --force -B ${BRANCH_NAME} origin/${BRANCH_NAME}
